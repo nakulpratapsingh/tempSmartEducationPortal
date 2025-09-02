bf62347f241ea8ff1b00c0f3a3e652d31a493152
nakulpratapsingh <pratapnakul60@gmail.com>
2025-09-02 18:32:40 +0530

    quizeComple

--- quizepage.js
+++ quizepage.js
@@ -1,28 +1,44 @@
 let questions = [];
 let current = 0;
 
+const urlParams = new URLSearchParams(window.location.search);
+const file = urlParams.get("subject");
+
+
+
 // JSON file load karna
-fetch("Aptitude.json")
+if (file)
+{
+fetch(file.endsWith(".json") ? file : `${file}.json`)
   .then(res => res.json())
   .then(data => {
     questions = data;
     showQuestion();
   });
+}
+else{
+  document.getElementById("question").innerHTML = "File not found!";
+}
 
 // ek question show karne ka function
 function showQuestion() {
   let q = questions[current];
 
+  //console.log(q);
   // question set karna
   document.getElementById("question").innerHTML = `Q${current + 1}. ${q.question}`;
 
   // options set karna
   let optionsHTML = q.options.map(opt => 
-    `<div><input type="radio" name="option"> ${opt}</div>`
-  ).join("");
+  `<div><input type="radio" name="option" value="${opt}"> ${opt}</div>`
+).join("");
+  
 
   document.getElementById("options").innerHTML = optionsHTML;
 }
+
+
+
 
 // next button
 function nextQuestion() {
